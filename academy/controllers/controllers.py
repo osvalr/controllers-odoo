93cd6a0e173b7ee4457ac9067b06edfa456ce555
Osval Reyes <osval@vauxoo.com>
2016-12-12 17:25:50 +0000

    [REF] academy: include biography to teachers

--- academy/controllers/controllers.py
+++ academy/controllers/controllers.py
@@ -10,6 +10,9 @@ class Academy(http.Controller):
             'teachers': teachers.search([])
         })
 
-    @http.route('/academy/<int:id>/', auth='public', website=True)
-    def teacher(self, id):
-        return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
+    @http.route('/academy/<model("academy.teachers"):teacher>', auth="public",
+                website=True)
+    def teacher(self, teacher):
+        return http.request.render('academy.biography', {
+            'person': teacher,
+        })
