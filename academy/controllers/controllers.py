b2208b4d47df1f55a534d2f074ab156ad894fd59
Osval Reyes <osval@vauxoo.com>
2016-12-12 16:42:15 +0000

    [REF] academy: set website true and include teacher data inside weblayout

--- academy/controllers/controllers.py
+++ academy/controllers/controllers.py
@@ -3,7 +3,7 @@ from odoo import http
 
 
 class Academy(http.Controller):
-    @http.route('/academy/academy/', auth='public')
+    @http.route('/academy/academy/', auth='public', website=True)
     def index(self, **kw):
         teachers = http.request.env['academy.teachers']
         return http.request.render('academy.index', {
