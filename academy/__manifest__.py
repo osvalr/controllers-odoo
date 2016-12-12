57d9677cd0e991719c93e2f69705d27e779c5260
Osval Reyes <osval@vauxoo.com>
2016-12-12 16:13:30 +0000

    [REF] academy: include website as dependency

--- academy/__manifest__.py
+++ academy/__manifest__.py
@@ -14,13 +14,17 @@
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # Check https://github.com/odoo/odoo/blob/master/
+    #       odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+        'base',
+        'website',
+    ],
 
     # always loaded
     'data': [
