0f3e234edb09c1b16265027eedc748cd9e1ae884
Osval Reyes <osval@vauxoo.com>
2016-12-12 21:44:11 +0000

    [REF] academy set mail and message system to courses

--- academy/__manifest__.py
+++ academy/__manifest__.py
@@ -22,7 +22,7 @@
 
     # any module necessary for this one to work correctly
     'depends': [
-        'base',
+        'mail',
         'website',
     ],
 
--- academy/models/models.py
+++ academy/models/models.py
@@ -17,13 +17,16 @@ from odoo import models, fields, api
 
 class Teachers(models.Model):
     _name = "academy.teachers"
+
     name = fields.Char()
     biography = fields.Html()
     course_ids = fields.One2many('academy.courses', 'teacher_id',
                                  string="Courses")
 
+
 class Courses(models.Model):
     _name = "academy.courses"
+    _inherit = "mail.thread"
 
     name = fields.Char()
     teacher_id = fields.Many2one('academy.teachers', string='Teacher')
