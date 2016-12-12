3413dbd5b562bc64a95120ea2ef702093f2e9fac
Osval Reyes <osval@vauxoo.com>
2016-12-12 16:08:59 +0000

    [REF] academy: include model Teachers

--- academy/controllers/controllers.py
+++ academy/controllers/controllers.py
@@ -5,8 +5,9 @@ from odoo import http
 class Academy(http.Controller):
     @http.route('/academy/academy/', auth='public')
     def index(self, **kw):
+        teachers = http.request.env['academy.teachers']
         return http.request.render('academy.index', {
-            'teachers': ['Diana Padilla', 'Jody Caroll', 'Lester Vaughn']
+            'teachers': teachers.search([])
         })
 
 #     @http.route('/academy/academy/objects/', auth='public')
